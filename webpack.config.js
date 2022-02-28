f57893fffb0b15ad213e871c8b53a42b31869d1b
KaitoTakase <k-takase@vectorinc.co.jp>
2022-02-28 16:12:57 +0900

    update: webpack.config.js
    モジュールをアップデートの際に更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const src = path.resolve(__dirname, 'src');
 const dist = path.resolve(__dirname, 'dist');
-
+const Sass = require('sass');
 // ビルドする際にHTMLも生成する
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // キャッシュパラメータを付与する
 const cacheParam = new Date().getTime().toString();
-// CSSを別ファイルに生成する
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// 画像をコピーする
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 // 古いファイルと未使用のファイルを削除するため
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const Sass = require('sass');
+// ファイルをコピーする
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// CSSを別ファイルに生成する
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//パッケージのライセンス情報をjsファイルの中に含める
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     // 'development' | 'production'
@@ -29,13 +30,21 @@ module.exports = {
         // 出力ファイル名
         filename: 'js/[name].bundle.js?[chunkhash:7]',
     },
+    //パッケージのライセンス情報をjsファイルの中に含める
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                extractComments: false,
+            }),
+        ],
+    },
     // ローカル開発用環境を立ち上げる
     devServer: {
-        open: true,
-        contentBase: src,
-        inline: true,
+        static: {
+            directory: src,
+        },
         hot: true,
-        watchContentBase: true,
         port: 3000,
     },
     // モジュールの解決方法を指定
@@ -47,25 +56,9 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /(\.s[ac]ss)$/, // 対象ファイルの拡張子
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: 'css-loader', options: { url: false } },
-                    { loader: 'postcss-loader' },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: Sass,
-                            // sassOptions: {
-                            //     fiber: Fiber,
-                            // },
-                        },
-                    },
-                ],
-            },
-            {
                 test: /\.(js|ts|tsx)$/,
-                exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+                // exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+                exclude: /node_modules\/(?!(dom7|ssr-window)\/).*/,
                 use: [
                     {
                         loader: 'babel-loader',
@@ -75,10 +68,24 @@ module.exports = {
                                     '@babel/preset-env',
                                     {
                                         useBuiltIns: 'usage',
-                                        corejs: '3.8',
+                                        corejs: '3.21.1',
                                     },
                                 ],
                             ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /(\.s[ac]ss)$/, // 対象ファイルの拡張子
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { url: false } },
+                    { loader: 'postcss-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: Sass,
                         },
                     },
                 ],
@@ -119,16 +126,25 @@ module.exports = {
     //   },
     // },
     plugins: [
+        new CleanWebpackPlugin(),
+        // CSSファイル名をつける
         new MiniCssExtractPlugin({
             filename: 'css/[name].css?[chunkhash:7]',
         }),
-        new CleanWebpackPlugin(),
         //コピーする対象を選択
         new CopyWebpackPlugin({
             patterns: [
                 {
                     from: path.resolve(src, 'img'),
                     to: path.resolve(dist, 'img'),
+                    toType: 'dir',
+                    globOptions: {
+                        ignore: ['*.DS_Store', '**/.gitkeep'],
+                    },
+                },
+                {
+                    from: path.resolve(src, 'static'),
+                    to: path.resolve(dist, 'static'),
                     toType: 'dir',
                     globOptions: {
                         ignore: ['*.DS_Store', '**/.gitkeep'],
@@ -146,10 +162,5 @@ module.exports = {
                 removeComments: true, // コメント削除、圧縮
             },
         }),
-        // その他
-        // new HtmlWebpackPlugin({
-        //   filename: "admin.html",
-        //   template: "admin.html",
-        // }),
     ],
 };
